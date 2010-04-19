d8801201f5128e43d2688e1875fbd2ece367c1b5
Eduardo Sasso <eduardo.sasso@gmail.com>
2010-04-19 11:29:03 -0300

    Inicio do codigo OO

--- refilmagem.js
+++ refilmagem.js
@@ -1,95 +1,70 @@
-$.YQL = function(url, callback) {
- 
-	query = 'select * from json where url="' + url + '"';
-
-    var encodedQuery = encodeURIComponent(query.toLowerCase()),
-        url = 'http://query.yahooapis.com/v1/public/yql?q='
-            + encodedQuery + '&format=json&callback=?';
- 
-    $.getJSON(url, callback);
- 
-};
-
-show_proximas_sessoes = function(cidade) {
-	var url = "http://beta.refilmagem.com.br/api/proximas-sessoes/" + cidade;
+var rfmg = {
+	url : "http://beta.refilmagem.com.br/api/",
 	
-	$.YQL(url, function(data) {
-	    sessoes =  data.query.results.json.nodes;
+	request: function(url, callback) {
+
+		query = 'select * from json where url="' + url + '"';
+
+		var encodedQuery = encodeURIComponent(query.toLowerCase()),
+		_url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodedQuery + '&format=json&callback=?';
+
+		$.getJSON(_url, callback);
+	},
+	
+	cinemas: function(cidade, callback){
+		 	var _this = this;
 		
-		// if (data.query.count == 1) {
-		// 	console.log(sessoes.title);
-		// 	return false;
-		// };
+			rfmg.url += cidade + "/cinemas";
+
+			rfmg.request(rfmg.url, function(data) {
+			    cinemas =  data.query.results.json.nodes;
+			
+				var _cinemas = new Array(); 
+
+				$.each(cinemas, function(i,item) {
+					var cinema = {  
+					  	nid : item.node_node_data_field_ref_cinema_nid,
+						nome : item.node_node_data_field_ref_cinema_title,
+						endereco : item.node_node_data_field_ref_cinema__node_revisions_body,
+					};
+					
+					_cinemas.push(cinema);
+				});
+
+				callback.call(_this,_cinemas);
+			});	
+	},
 	
-		$.each(sessoes, function(i,item) {
-			sessao = item.hora + " › " + item.title;
-			class_ = cidade + " " + item.hora;
-			$('#proximas-sessoes ul').append('<li class="arrow"><a href="#cinemas" id="' + item.nid + ' " class="'+ class_ + '">' + sessao +'</a></li>');
-		});
+	get_distancia: function(endereco_inicio, endereco_fim, callback) {
+	 	var _this = this;
 	
-	});
+		var google_maps = "http://maps.google.com/maps/nav?key=ABQIAAAAzr2EBOXUKnm_jVnk0OJI7xSosDVG8KKPE1-m51RBrvYughuyMxQ-i1QfUnH94QxWIa6N4U6MouMmBA&output=js&doflg=ptj&q=";
+		google_maps += "from:" + escape(endereco_inicio + ' ');
+		google_maps += "to:" + escape(endereco_fim);
+		
+		rfmg.request(google_maps, function(data) {
+			result = data.query.results.json;
+			directions = result.Directions;
+			status = result.Status;
+			
+			 callback.call(_this,result);
+		});	
+	} 	
 }
 
-show_cinemas_filme = function(nid, cidade, horario){
-	url = "http://beta.refilmagem.com.br/api/filme-cinemas/" + nid + "/" + cidade + "/" + horario;
-	
-	$.YQL(url, function(data) {
-		console.log(data);
-	    cinemas =  data.query.results.json.nodes
-			console.log(cinemas.length);
-	
-		if ($.isArray(cinemas) == false) {
-			$('#cinemas ul').append('<li>' + cinemas.node_node_data_field_ref_cinema_title + '</li>');
-			return false;
-		};
 
-		$.each(cinemas, function(i,item) {
-			$('#cinemas ul').append('<li>' + item.node_node_data_field_ref_cinema_title + '</li>');
+$(function (){
+	onde_estou = '-29.9986925,-51.1487349';
+	
+	rfmg.cinemas('porto-alegre',function(data){
+		$.each(data, function(key, cinema) { 
+			
+			rfmg.get_distancia(onde_estou, cinema.endereco, function(distancia){
+				//todo 602 nao vem com metros, fazer nova chamada para pegar o endereco certo
+				if (distancia.Status.code == 200) {
+					cinema.distancia = distancia.Directions.Distance.meters;
+				};
+			});
 		});
-	
 	});
-	
-}
-
-get_cidades = function(){
-	var url = "http://beta.refilmagem.com.br/api/cidades";
-
-	$.YQL(url, function(data) {
-	    cidades =  data.query.results.json.nodes;
-	
-		$.each(cidades, function(i,item) {
-			$('#home ul').append('<li class="arrow"><a href="#proximas-sessoes" id="' + item.tid + '">' + item.term_data_name +'</a></li>');
-
-		});
-	
-	});
-	
-}
-jQuery(document).ready(function($) {
-	get_cidades();
-	
-	$('#cidades li a').live('click', function(){
-		cidade = $(this).attr('id');
-
-		$('#proximas-sessoes ul').empty();
-		show_proximas_sessoes(cidade);
-		
-		//console.log($(this).attr('id'));
-	});
-	
-	$('#proximas-sessoes ul li a').live('click', function(){
-		$('#cinemas h1').text($(this).text());
-
-		nid = $(this).attr('id');
-		args = $(this).attr('class');
-		
-		args = args.split(' ');
-
-		$('#cinemas ul').empty();
-		show_cinemas_filme(nid, args[0], args[1]);
-
-		//console.log($(this).attr('id'));
-	});
-	
-	
 });
